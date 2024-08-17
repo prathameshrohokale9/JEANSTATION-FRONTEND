df61c79e2dc8670ca53592b5176437b9b25ab009
prathameshrohokale9 <110075898+prathameshrohokale9@users.noreply.github.com>
2024-08-17 23:58:53 +0530

    Add files via upload

---
+++ src/components/Carts/NavbarGeneric.jsx
@@ -0,0 +1,44 @@
+import React, { useState } from 'react';
+import {Link,useNavigate} from 'react-router-dom'
+export default function NavbarGeneric({ onSearch }) {
+  const [searchTerm, setSearchTerm] = useState('');
+  const navigate = useNavigate();
+  const [minprice , setMinPrice]= useState('');
+  const [maxprice , setMaxPrice] = useState('');
+ 
+  // const handleprice=()=>{
+  //   localStorage.setItem('minprice', minprice);
+  //   localStorage.setItem('maxprice', maxprice);
+  //   navigate(`/searchbyprice/${minprice}/${maxprice}`)
+  // }
+ 
+  
+
+ 
+  const handleSearch = (e) => {
+    localStorage.setItem('searchTerm',searchTerm)
+    navigate(`/searchbyname/${searchTerm}`)
+ 
+  };
+ 
+  return (
+<nav className="navbar bg-body-tertiary">
+<div className="container-fluid">
+<img src={require("../../images/Home/js_logo.jpg")} alt="js_logo" width="40" height="32" />
+<div className="d-flex"  >
+<input
+            className="form-control me-2"
+            type="search"
+            placeholder="Search"
+            aria-label="Search"
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
+          />
+<button className="btn btn-outline-success" type="submit" onClick={handleSearch}>Search</button>
+</div>
+
+</div>
+</nav>
+  );
+}
+
